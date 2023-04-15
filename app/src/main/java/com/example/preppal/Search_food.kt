1172f46b7ed874388103ce6ad7894639d13d1c8d
Dananjayan <107125445+AkilanD04@users.noreply.github.com>
2023-04-16 04:24:19 +0530

    fixed bugs

--- app/src/main/java/com/example/preppal/Search_food.kt
+++ app/src/main/java/com/example/preppal/Search_food.kt
@@ -1,6 +1,7 @@
 package com.example.preppal
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -28,32 +29,43 @@ class Search_food : AppCompatActivity() {
             if (name_in.text.isEmpty()) {
                 Toast.makeText(this, "Please enter something ", Toast.LENGTH_SHORT).show()
             } else {
-                add_name(name_in)
+                if (name_in.text.toString().toIntOrNull() == null){
+                    add_name(name_in,this)
+                }
+                else{
+                    Toast.makeText(this, "Enter a valid ingredient/name", Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
     }
 
-    private fun add_name(name_in: EditText) {
+    private fun add_name(name_in: EditText, context: Context) {
         out.text = ""
         val db = Room.databaseBuilder(this, AppDatabase::class.java, "MealDatabase").build()
         val mealDao = db.mealDao()
         runBlocking {
             launch {
                 val meal_list: List<Meals> = mealDao.id_return(name_in.text.toString())
-                for (m in meal_list) {
-                    out.append(
-                        "\nmeal id: ${m.id} \nmeal name: ${m.Meal} \nDrinkAlternate: ${m.DrinkAlternate} \nCategory: ${m.Category} \narea: ${m.Area} \ninstructions: ${m.Instructions}" +
-                                "\nmeal thumb: ${m.MealThumb} \ntags: ${m.Tags} \nyoutubeLink: ${m.YoutubeLink} \ningredient1: ${m.Ingredient1} \ningredient2: ${m.Ingredient2}" +
-                                "\ningredient3: ${m.Ingredient3} \ningredient4: ${m.Ingredient4} \ningredient5: ${m.Ingredient5} \ningredient6: ${m.Ingredient6} \ningredient7: ${m.Ingredient6}" +
-                                "\ningredient8: ${m.Ingredient8} \ningredient9: ${m.Ingredient9} \ningredient10: ${m.Ingredient10} \ningredient11: ${m.Ingredient11} \ningredient12: ${m.Ingredient12}" +
-                                "\ningredient13: ${m.Ingredient13} \ningredient14: ${m.Ingredient14} \ningredient15: ${m.Ingredient15} \ningredient16: ${m.Ingredient16} \ningredient17: ${m.Ingredient17}" +
-                                "\ningredient18: ${m.Ingredient18} \ningredient19: ${m.Ingredient19} \ningredient20: ${m.Ingredient20} \nmeasure1: ${m.Measure1} \nmeasure2: ${m.Measure2} \nmeasure3: ${m.Measure3}" +
-                                "\nmeasure4: ${m.Measure4} \nmeasure5: ${m.Measure5} \nmeasure6: ${m.Measure6} \nmeasure7: ${m.Measure7} \nmeasure8: ${m.Measure8} \nmeasure9: ${m.Measure9} \nmeasure10: ${m.Measure10}" +
-                                "\nmeasure11: ${m.Measure11} \nmeasure12: ${m.Measure12} \nmeasure13: ${m.Measure13} \nmeasure14: ${m.Measure14} \nmeasure15: ${m.Measure15} \nmeasure16: ${m.Measure16}" +
-                                "\nmeasure17: ${m.Measure17} \nmeasure18: ${m.Measure18} \nmeasure19: ${m.Measure19} \nmeasure20: ${m.Measure20}"
-                    )
-                    out.append("\n\n")
+                if (meal_list.isEmpty()){
+                    runOnUiThread {
+                        Toast.makeText(context, "Ingredient not found", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    for (m in meal_list) {
+                        out.append(
+                            "\nmeal id: ${m.id} \nmeal name: ${m.Meal} \nDrinkAlternate: ${m.DrinkAlternate} \nCategory: ${m.Category} \narea: ${m.Area} \ninstructions: ${m.Instructions}" +
+                                    "\nmeal thumb: ${m.MealThumb} \ntags: ${m.Tags} \nyoutubeLink: ${m.YoutubeLink} \ningredient1: ${m.Ingredient1} \ningredient2: ${m.Ingredient2}" +
+                                    "\ningredient3: ${m.Ingredient3} \ningredient4: ${m.Ingredient4} \ningredient5: ${m.Ingredient5} \ningredient6: ${m.Ingredient6} \ningredient7: ${m.Ingredient6}" +
+                                    "\ningredient8: ${m.Ingredient8} \ningredient9: ${m.Ingredient9} \ningredient10: ${m.Ingredient10} \ningredient11: ${m.Ingredient11} \ningredient12: ${m.Ingredient12}" +
+                                    "\ningredient13: ${m.Ingredient13} \ningredient14: ${m.Ingredient14} \ningredient15: ${m.Ingredient15} \ningredient16: ${m.Ingredient16} \ningredient17: ${m.Ingredient17}" +
+                                    "\ningredient18: ${m.Ingredient18} \ningredient19: ${m.Ingredient19} \ningredient20: ${m.Ingredient20} \nmeasure1: ${m.Measure1} \nmeasure2: ${m.Measure2} \nmeasure3: ${m.Measure3}" +
+                                    "\nmeasure4: ${m.Measure4} \nmeasure5: ${m.Measure5} \nmeasure6: ${m.Measure6} \nmeasure7: ${m.Measure7} \nmeasure8: ${m.Measure8} \nmeasure9: ${m.Measure9} \nmeasure10: ${m.Measure10}" +
+                                    "\nmeasure11: ${m.Measure11} \nmeasure12: ${m.Measure12} \nmeasure13: ${m.Measure13} \nmeasure14: ${m.Measure14} \nmeasure15: ${m.Measure15} \nmeasure16: ${m.Measure16}" +
+                                    "\nmeasure17: ${m.Measure17} \nmeasure18: ${m.Measure18} \nmeasure19: ${m.Measure19} \nmeasure20: ${m.Measure20}"
+                        )
+                        out.append("\n\n")
+                    }
                 }
             }
         }
